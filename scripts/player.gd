6c9076c89d4932362140e6930cef13f0e8dfcaf0
voidx <void.ecs@gmail.com>
2017-07-31 20:34:37 +0800

    add wall grabing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
+var Wall = load('res://scripts/Wall.gd')
+var Ground = load('res://scripts/Ground.gd')
+
 export var gravity = 1000
 export var acceleration = 2000
 export var deceleration = 4000
 export var maxSpeed = 500
+export var speedOnWall = 100
 export var jumpForce = 800
 export var maxJump = 2
 
@@ -11,6 +15,24 @@ var velocity = Vector2(0, 0)
 var moveLeft = false
 var moveRight = false
 var jumpCount = 0
+var jumping = false
+var falling = true
+var onWall = false
+
+var playerState = 'in-air'
+
+func jump():
+  if jumpCount < maxJump or onWall:
+    velocity.y = -jumpForce
+    jumpCount += 1
+    playerState = 'in-air'
+    jumping = true
+
+    if onWall:
+      if moveRight:
+        velocity.x = -jumpForce * 0.6
+      elif moveLeft:
+        velocity.x = jumpForce * 0.6
 
 func _ready():
   set_fixed_process(true)
@@ -20,23 +42,55 @@ func _unhandled_input(event):
   if event.is_action_pressed('move_left'):
     moveLeft = true
     moveRight = false
+    prints('LEFT +', moveLeft, moveRight)
   elif event.is_action_released('move_left'):
     moveLeft = false
     if Input.is_action_pressed('move_right'): moveRight = true
+    prints('Left -', moveLeft, moveRight)
   elif event.is_action_pressed('move_right'):
-    print('RIGHT')
     moveRight = true
     moveLeft = false
+    prints('RIGHT +', moveLeft, moveRight)
   elif event.is_action_released('move_right'):
     moveRight = false
     if Input.is_action_pressed('move_left'): moveLeft = true
-  elif event.is_action_pressed('jump'):
-    if jumpCount < maxJump:
-      velocity.y = -jumpForce
-      jumpCount += 1
+    prints('RIGHT -', moveLeft, moveRight)
+  elif event.is_action_pressed('jump'): jump()
 
 func _fixed_process(delta):
+  if playerState == 'landed':
+    landedState(delta)
+  elif playerState == 'in-air':
+    inAirState(delta)
+
+func landedState(delta):
+  if moveLeft:
+    velocity.x = max(velocity.x - acceleration * delta, -maxSpeed)
+  elif moveRight:
+    velocity.x = min(velocity.x + acceleration * delta, maxSpeed)
+  else:
+    if velocity.x > 0:
+      velocity.x = max(0, velocity.x - deceleration * delta)
+    elif velocity.x < 0:
+      velocity.x = min(0, velocity.x + deceleration * delta)
+
+  velocity.y = 0
+
+  var movement = velocity * delta
+  var remainder = move(movement)
+
+  if is_colliding():
+    var normal = get_collision_normal()
+    movement = normal.slide(remainder)
+    velocity = normal.slide(velocity)
+    move(movement)
+
+func inAirState(delta):
   velocity.y = min(velocity.y + gravity * delta, maxSpeed)
+
+  if velocity.y > 0:
+    jumping = false
+    falling = true
 
   if moveLeft:
     velocity.x = max(velocity.x - acceleration * delta, -maxSpeed)
@@ -51,9 +105,30 @@ func _fixed_process(delta):
   var movement = velocity * delta
   var remainder = move(movement)
 
-  while is_colliding():
-    jumpCount = 0
-    var normal = get_collision_normal()
-    movement = normal.slide(remainder)
-    velocity = normal.slide(velocity)
-    remainder = move(movement)
+  onWall = false
+
+  if is_colliding():
+    var collider = get_collider()
+    
+    if collider extends Ground:
+      playerState = 'landed'
+      jumpCount = 0
+      falling = false
+      var normal = get_collision_normal()
+      velocity = normal.slide(velocity)
+      movement = normal.slide(remainder)
+      move(movement)
+    elif collider extends Wall:
+      if jumping:
+        var normal = get_collision_normal()
+        velocity = normal.slide(velocity)
+        movement = normal.slide(remainder)
+        move(movement)
+      elif falling:
+        onWall = true
+        velocity.x = 0
+        if velocity.y > speedOnWall:
+          velocity.y = speedOnWall
+        else:
+          velocity.y = lerp(velocity.y, speedOnWall, delta)
+        move(velocity * delta)
